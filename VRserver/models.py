f4b81a377989897f886f6e23cfeed9f0334717b8
Shen Yi Chen <kuya5220@gmail.com>
2022-04-23 17:16:17 +0800

    Modified the database format, API functions.

--- VRserver/models.py
+++ VRserver/models.py
@@ -1,16 +1,10 @@
 from django.db import models
 
-
 # Create your models here.
-class StatusDB(models.Model):
+class User(models.Model):
     id = models.AutoField(primary_key=True)
     created = models.DateTimeField(auto_now_add=True)
-    user1 = models.TextField()
-    user2 = models.TextField()
-    user3 = models.TextField()
-    user4 = models.TextField()
-    user5 = models.TextField()
-    user6 = models.TextField()
-
+    name = models.TextField(blank=False, null=False)
+    status = models.TextField(blank=False, null=False)
     class Meta:
         db_table = "user_table"  # 資料庫內表的名稱  #+ __init__ >create tazble name
--- VRserver/serializers.py
+++ VRserver/serializers.py
@@ -1,9 +1,9 @@
-from VRserver.models import StatusDB
+from VRserver.models import User
 from rest_framework import serializers
 
 
-class StatusSerializer(serializers.ModelSerializer):
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
-        model = StatusDB
+        model = User
         fields = '__all__'
         # serializer_choice_field = ('user1', 'user2', 'user3', 'user4', 'user5', 'user6')
